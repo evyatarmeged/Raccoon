40e79fcc77e745a919dc0a7ef6110b3c0cd5bb95
evyatarmeged <evyatarmeged@gmail.com>
2018-05-21 23:52:11 +0300

    tls

--- tls.py
+++ tls.py
@@ -1,4 +1,5 @@
 from subprocess import Popen, PIPE
+from threading import Thread
 
 
 class TLSCipherSuiteChcker:
@@ -13,7 +14,8 @@ class TLSCipherSuiteChcker:
         parsed = self.parse_nmap_outpt(result, err)
         return parsed
 
-    def parse_nmap_outpt(self, result, err):
+    @staticmethod
+    def parse_nmap_outpt(result, err):
         if err:
             return str(err, encoding='ascii')
         else:
@@ -24,16 +26,41 @@ class TLSCipherSuiteChcker:
 class TLSVersionChecker:
     def __init__(self, host):
         self.host = host
-        self.versions = ('tls1', 'tls1_1', 'tls1_2')
-        self.script = tuple("openssl s_client -connect {}:443".format(self.host).split())
+        self.versions = {
+            "tls1": "TLSv1.0",
+            "tls1_1": "TLSv1.1",
+            "tls1_2": "TLSv1.2"
+        }
+        self.base_script = "openssl s_client -connect {}:443 ".format(self.host)
 
     def test_supported_versions(self):
         sni = self._test_sni()
-        non_sni = self._test_non_sni()
+        # non_sni = self._test_non_sni()
+
+    def _exec_openssl(self, script):
+        # TODO: Overwrite with Popen array
+        # commands = [ for v in self.versions]
+        # procs = [Popen(cmd.split(), stdout=PIPE, stderr=PIPE) for cmd in commands]
+        for v in self.versions.keys():
+            proc = Popen(script + ' -{}'.format(v), stdout=PIPE, stderr=PIPE)
+            p.wait()
+            result, err = p.communicate()
+            self._parse_openssl_output(result, err)
 
     def _test_sni(self):
-        pass
+        self._exec_openssl(self.base_script + ' -servername {}'.format(self.host))
 
     def _test_non_sni(self):
         pass
 
+    @staticmethod
+    def _parse_openssl_output(result, err):
+        if err:
+            return str(err, encoding='ascii')
+        else:
+            result = str(result, encoding='ascii').split('\n')
+            return '\n'.join([line for line in result if "TLS" in line or "ciphers" in line])
+
+
+a = TLSVersionChecker('www.walla.co.il')
+a.test_supported_versions()
