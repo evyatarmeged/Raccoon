369051ce88de3bf6ff53422e89b20a13008ceae4
evyatarmeged <evyatarmeged@gmail.com>
2018-05-25 12:44:46 +0300

    timeout added to openssl calls. more WAFs added

--- tls.py
+++ tls.py
@@ -7,7 +7,6 @@ class TLSCipherSuiteChecker:
         self.host = host
 
     def scan_ciphers(self, port=443):
-        # TODO: should be threaded
         script = "nmap --script ssl-enum-ciphers -p {} {}".format(str(port), self.host)
         process = Popen(script.split(), stdout=PIPE, stderr=PIPE)
         result, err = process.communicate()
@@ -31,8 +30,9 @@ class TLSVersionChecker:
 
     def __init__(self, host):
         self.host = host
-        self.versions = ("tls1", "tls1_1", "tls1_2")
-        self.base_script = "openssl s_client -connect {}:443 ".format(self.host)
+        self._versions = ("tls1", "tls1_1", "tls1_2")
+        # OpenSSL likes to hang, Linux timeout to the rescue
+        self._base_script = "timeout 7 openssl s_client -connect {}:443 ".format(self.host)
 
     def test_supported_versions(self):
         # TODO: should be threaded
@@ -44,38 +44,34 @@ class TLSVersionChecker:
     def _exec_openssl(self, script):
         procs = []
         outputs = []
-        for v in self.versions:
+        for v in self._versions:
             curr = script + ' -{}'.format(v)
             procs.append(Popen(curr.split(), stdout=PIPE, stderr=PIPE))
         for p in procs:
             p.wait()
             result, err = p.communicate()
-            result = str(result, encoding='ascii')
-            outputs.append(result)
+            outputs.append(str(result, encoding='ascii'))
         return outputs
 
     def _test_sni(self):
-        outputs = self._exec_openssl(self.base_script + ' -servername {}'.format(self.host))
+        outputs = self._exec_openssl(self._base_script + ' -servername {}'.format(self.host))
         return self._parse_openssl_output(outputs)
 
     def _test_non_sni(self):
-        outputs = self._exec_openssl(self.base_script)
+        outputs = self._exec_openssl(self._base_script)
         return self._parse_openssl_output(outputs)
 
     def _parse_openssl_output(self, results):
-        is_supported = {}
+        is_supported = {"TLSv1": False, "TLSv1.1": False, "TLSv1.2": False}
         for res in results:
-            if "CONNECTED" in res:
-                for line in res.split('\n'):
-                    if "Protocol" in line:
-                        ver = line.strip().split(':')[1].strip()
-                        is_supported[ver] = True
+            if "no peer certificate available" in res:
+                continue
+            for line in res.split('\n'):
+                if "Protocol" in line:
+                    ver = line.strip().split(':')[1].strip()
+                    is_supported[ver] = True
         return is_supported
 
     def run(self):
         # Thread Method
         pass
-
-
-a = TLSVersionChecker('www.israelpost.co.il')
-print(a.test_supported_versions())
--- waf.py
+++ waf.py
@@ -2,19 +2,24 @@ from fake_useragent import UserAgent
 import requests
 
 
+SERVER = "Server"
+
+
 class WAFApplicationMethods:
 
     @staticmethod
     def detect_cloudfront(headers):
-        service = "cloudfront"
-        # TODO: Solve current TypeError
-        if service in headers.get('via') or service in headers.get('x-cache')\
-                or headers.get("Server") == "CloudFront":
+        service = "CloudFront"
+        waf_headers = ("Via", "X-cache")
+        if any(h in headers.keys() for h in waf_headers) and any(service.lower() in val for val in headers.values()):
+            return True
+
+        if headers.get(SERVER) == service:
             return True
 
     @staticmethod
     def detect_incapsula(headers):
-        if "x-iinfo" in headers.keys() or headers.get("x-cdn") == "Incapsula":
+        if "X-Iinfo" in headers.keys() or headers.get("X-CDN") == "Incapsula":
             return True
 
     @staticmethod
@@ -24,7 +29,15 @@ class WAFApplicationMethods:
 
     @staticmethod
     def detect_cloudflare(headers):
-        pass
+        return "CF-RAY" in headers.keys() or headers.get(SERVER) == "cloudfront"
+
+    @staticmethod
+    def detect_edgecast(headers):
+        return SERVER in headers.keys() and "ECD" in headers[SERVER]
+
+    @staticmethod
+    def detect_maxcdn(headers):
+        return SERVER in headers.keys() and "NetDNA-cache" in headers[SERVER]
 
 
 class WAF:
@@ -32,7 +45,10 @@ class WAF:
     WAF_CNAME_MAP = {
         "incapdns": "Incapsula",
         "edgekey": "Akamai",
-        "distil": "Distil Networks"
+        "distil": "Distil Networks",
+        "cloudfront": "CloudFront",
+        "adn": "EdgeCast",
+        "netdna-cdn": "MaxCDN"
     }
 
     def __init__(self, host, dns_records):
@@ -48,13 +64,11 @@ class WAF:
         for waf in self.WAF_CNAME_MAP:
             if any(waf in cname for cname in self.cnames):
                 print("Detected WAF presence in CNAME: {}".format(self.WAF_CNAME_MAP.get(waf)))
-            else:
-                print("No WAF presence detected in CNAME")
 
     def _detect_by_application(self, ua):
+        # TODO: Add HTTP status code sanitation
         ua = ua.random
         response = requests.head('http://{}'.format(self.host), headers={'User-Agent': ua})
         for method in WAFApplicationMethods.__dict__:
             if callable(getattr(WAFApplicationMethods, method)):
                 print(getattr(WAFApplicationMethods, method)(response.headers))
-
