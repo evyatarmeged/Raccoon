fba114b2f338e29f17a0d040dbc8de772acb8031
evyatarmeged <evyatarmeged@gmail.com>
2018-09-08 22:20:48 +0300

    dns dumpster mapping bug fix (#18). version 0.8.3

--- raccoon_src/main.py
+++ raccoon_src/main.py
@@ -44,7 +44,7 @@ https://github.com/evyatarmeged/Raccoon
 
 
 @click.command()
-@click.version_option("0.8.2")
+@click.version_option("0.8.3")
 @click.option("-t", "--target", required=True, help="Target to scan")
 @click.option("-d", "--dns-records", default="A,MX,NS,CNAME,SOA,TXT",
               help="Comma separated DNS records to query. Defaults to: A,MX,NS,CNAME,SOA,TXT")
